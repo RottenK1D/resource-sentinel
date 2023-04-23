bb3dac088133e06538e805ad97d91d71c5f60936
nuker <rinards.sondors@gmail.com>
2023-04-23 19:14:37 +0100

    added system info crate and implemented logic

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
-use axum::{routing::get, Router, Server};
+use axum::{routing::get, Router, Server, extract::State};
+use sysinfo::{System, SystemExt, CpuExt};
+use std::{fmt::Write, sync::{Arc, Mutex}};
 
 #[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(root));
+    let router = Router::new()
+        .route("/", get(root))
+        .with_state(AppState {
+            sys: Arc::new(Mutex::new(System::new()))
+        });
+
     let server = Server::bind(&"0.0.0.0:3000".parse().unwrap())
                         .serve(router.into_make_service());
 
@@ -11,7 +18,23 @@ async fn main() {
     server.await.unwrap();
 }
 
+#[derive(Debug,Clone)]
+struct AppState {
+    sys: Arc<Mutex<System>>,
+}
 
-async fn root() -> &'static str {
-    "HELLO AXUM"
+
+async fn root(State(state): State<AppState>) -> String{
+    let mut s = String::new();   
+    let mut sys = state.sys.lock().unwrap();
+    sys.refresh_cpu();
+
+    for (i, cpu) in sys.cpus().iter().enumerate() {
+        let i  = i + 1;
+        
+        let usage =  cpu.cpu_usage();
+        writeln!(&mut s , "CPU {i} {usage}%").unwrap();
+    }
+    
+    return s;
 }
